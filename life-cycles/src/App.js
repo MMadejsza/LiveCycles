e9b701e88d4df2a7113bc29452cc60413c1cd028
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-09-01 13:43:00 +0100

    fetch

--- life-cycles/src/App.js
+++ life-cycles/src/App.js
@@ -6,18 +6,30 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      comments: [],
     }
 
   }
 
   componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          comments: res.filter(singleComment => singleComment.id <= 10)
+        })
+      })
+
+    console.log(this.state);
 
   }
 
   render() {
     return (
-      <div>
+      <div className="container">
+        <ol>
+          { }
+        </ol>
 
       </div>
     )
