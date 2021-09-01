6b04a349ef6292839626d8f231343d4acecbfa0b
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-09-01 16:04:00 +0100

    comments from state as undefined

--- life-cycles/src/App.js
+++ life-cycles/src/App.js
@@ -17,14 +17,24 @@ class App extends Component {
         const response = res.filter(singleComment => singleComment.id <= 10)
         this.setState({
           comments: [...response],
+          prevComments: ''
         })
       })
+  }
+
+  deleteComment = () => {
+    this.setState({
+      comments: this.prevState.comments.pop(),
+      prevComments: this.prevState.comments
+    })
+    console.log(this.state)
   }
 
   render() {
     const commentsGroup = this.state.comments;
     return (
       <div className='containerup'>
+        <button className='btn' onClick={this.deleteComment}>Delete comment</button>
         <ol className="container">
           {commentsGroup.map(eachComment => {
             return <li key={eachComment.id} className='singleComment'>
